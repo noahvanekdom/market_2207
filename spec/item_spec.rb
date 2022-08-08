e0ac330d1544becae13858226c12f5ef9d8a53ee
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-08 09:06:50 -0600

    Add initialize tests for item and vendor

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,14 @@
 require './lib/item'
 
 describe Item do
-    
+    let(:item1) { Item.new({name: 'Peach', price: "$0.75"}) }
+    let(:item2) { Item.new({name: 'Tomato', price: '$0.50'}) }
+
+    describe 'initialize' do
+        it 'has a name' do
+        end
+
+        it 'has a price' do
+        end
+    end
 end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -2,4 +2,15 @@ require './lib/item'
 require './lib/vendor'
 
 describe Vendor do
+    let(:item1) { Item.new({name: 'Peach', price: "$0.75"}) }
+    let(:item2) { Item.new({name: 'Tomato', price: '$0.50'}) }
+
+    describe 'initialize' do
+        it 'has a name' do
+        end
+
+        it 'has a price' do
+        end
+
+
 end
